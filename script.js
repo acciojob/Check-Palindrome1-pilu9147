cf285a9b1ada8ff47c67d3519d0ef0d635347b77
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-05-14 19:33:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // complete the given function
 
 function palindrome(str){
-	str.toLowerCase()
+var s =	str.toLowerCase()
 var i = 0
-	var j = str.length - 1;
+	var j = s.length - 1;
 	while (i<j) {
-		if(str.charAt(i)===str.charAt(j)) {
+		if(s.charAt(i)===s.charAt(j)) {
 			i++;
 			j--;
 		}
