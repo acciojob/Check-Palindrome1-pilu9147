57c6f65d97e854fa5144387593cb13c5e2de81df
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-05-14 19:15:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 // complete the given function
 
 function palindrome(str){
-var i = 0
-	var j = str.length - 1;
-	while (i<j) {
-		if(str.charAt(i)===str.charAt(j)) {
-			i++;
-			j--;
-		}
-		else {
-			return false
-		}
+	 const len =s.length;
+
+    
+    for (let i = 0; i < len / 2; i++) {
+
+
+        if (s[i] !== s[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+// var i = 0
+// 	var j = str.length - 1;
+// 	while (i<j) {
+// 		if(str.charAt(i)===str.charAt(j)) {
+// 			i++;
+// 			j--;
+// 		}
+// 		else {
+// 			return false
+// 		}
 		
-	}
-	return true
+// 	}
+// 	return true
 }
 module.exports = palindrome
